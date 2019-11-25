4d9f8fcd9d1cfcda2bddcdc2330c13cfb1b3db4d
haoshuai <haoshuai9192@gmail.com>
2019-11-25 11:14:09 +0800

    初步完成显示空间

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -1,71 +1,53 @@
-// pages/info/info.js
+// pages/edit/edit.js
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    image:"../../resource/image/touxiang.jpeg",
-    name:"我是谁",
-    array: [
-      {phone:"1868225123"},
-      {phone:"1868225123"}
-  ],
-    remarks: "我就是备注"
+    // flase 增加 true 编辑
+    type: false,
+    name: "",
+    image: "../../resource/image/touxiang.jpeg",
+    phone: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
-  },
+  onLoad: function(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-
-  },
+  onReady: function() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
-  },
+  onShow: function() {},
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-
-  },
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-
-  },
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
-  },
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-
-  },
+  onReachBottom: function() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
-  }
-})
+  onShareAppMessage: function() {}
+});
