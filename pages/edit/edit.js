96f95be742bd4d773a37c34f35206044925c91de
haoshuai <haoshuai9192@gmail.com>
2019-12-17 10:26:38 +0800

    电话号码新增可以

--- pages/edit/edit.js
+++ pages/edit/edit.js
@@ -13,7 +13,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    console.log("设置参数", options.is_add)
+    // console.log("设置参数", options.is_add)
     var that = this
     if (options.is_add) {
       that.setData({
@@ -64,5 +64,26 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {}
+  onShareAppMessage: function() {},
+  
+  save_info_data:function(){
+    console.log("保存联系人")
+  },
+
+  bindFormSubmit: function (e) {
+    console.log("开始提交数据")
+    console.log(e.detail.value)
+  },
+  add_new_phone:function() {
+    console.log("新增电话")
+    var that = this
+    var info_data = that.data.info_data
+    var new_number = { type: "", phone_number: "" }
+    info_data.array.push(new_number)
+
+    console.log("老的数据", info_data)
+    that.setData({
+      info_data: info_data
+    });
+  }
 });
